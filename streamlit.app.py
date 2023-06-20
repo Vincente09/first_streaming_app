76e4be09425d6fa6a393d6954c0b67fd326fc341
Vincente09 <137042000+Vincente09@users.noreply.github.com>
2023-06-21 07:56:10 +0800

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -59,13 +59,15 @@ if streamlit.button ('Get Fruit Load List'):
 
 def insert_row_snowflake(new_fruit):
      with my_cnx.cursor() as my_cur:  
-          my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('from streamlit')")
+          my_cur.execute("INSERT INTO FRUIT_LOAD_LIST VALUES ('jackfruit')")
           return "Thank your for adding " + new_fruit
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if  streamlit.button ( 'Add a Fruit To the list'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
     streamlit.text(back_from_function)
+
+
 
 
 #from urllib.error import URL error
