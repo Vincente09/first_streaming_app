0c5bd49265a8d060f7a002081bdef808e39515fd
Vincente09 <137042000+Vincente09@users.noreply.github.com>
2023-06-20 19:13:53 +0800

    Update streamlit.app.py

--- streamlit.app.py
+++ streamlit.app.py
@@ -48,6 +48,7 @@ streamlit.dataframe(my_data_rows)
 
 
 fruit_choice = streamlit.text_input('What fruit would you like to add?')
-streamlit.write('Thanks for adding, fruit_choice')
+streamlit.button('add word)
+streamlit.write( 'has been added')
 
 
